d5615429716e81ef5f1810ea37579e95c74ba74d
Bahodir Amerbekov <sizning_email@example.com>
2026-06-04 10:58:54 +0500

    Fix property search

--- server/public/script.js
+++ server/public/script.js
@@ -1,8 +1,12 @@
 const fetchProperties = async (range, bedroom) => {
   const params = new URLSearchParams();
+
   params.append("range", range);
   params.append("bedroom", bedroom);
-const res = await fetch(`/property?${params}`);
+
+  const res = await fetch(`/property?${params}`);
+  const data = await res.json();
+
   renderPropertiesUI(data);
   initialPrice(data);
 };
@@ -11,16 +15,22 @@ fetchProperties("[0,10000]", "any");
 
 const renderPropertiesUI = (properties) => {
   const contDiv = document.getElementById("main-content");
-  contDiv.innerHTML = ``;
+
+  contDiv.innerHTML = "";
 
   const propertyCards = properties.map((property) => {
     const div = document.createElement("div");
+
     div.classList.add("property-card");
+
     div.innerHTML = `
-<img src="/${property.image}" alt="Property Image" />            <h3>${property.name}</h3>
-            <p>$${property.price} / month</p>
-        </div>
-        `;
+      <img src="/${property.image}" alt="Property Image" />
+      <div class="info">
+        <h3>${property.name}</h3>
+        <p>$${property.price} / month</p>
+      </div>
+    `;
+
     return div;
   });
 
@@ -32,17 +42,22 @@ const initialPrice = (properties) => {
 
   const minPrice = Math.min(...prices);
   const maxPrice = Math.max(...prices);
+
   document.getElementById("min-price").value = minPrice;
   document.getElementById("max-price").value = maxPrice;
 };
 
 document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
+
   const formData = new FormData(e.target);
-  const range = `[${formData.get("min-price")},${formData.get("max-price")}]`;
+
+  const minPrice = formData.get("min-price") || 0;
+  const maxPrice = formData.get("max-price") || 10000;
+
+  const range = `[${minPrice},${maxPrice}]`;
+
   const bedroom = formData.get("bedroom");
-  console.log(bedroom);
-  console.log(range);
 
   fetchProperties(range, bedroom);
 });
